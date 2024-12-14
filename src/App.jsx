c62dce593bdc7ab4d367d65e48cfcbabf042b787
Graham4914 <67054735+Graham4914@users.noreply.github.com>
2024-12-14 12:43:05 +0800

    made layout changes to allow for the cv preview and the editing section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from 'react'
 import GeneralInfoSection from './compnents/GeneralInfoSection';
 import EducationSection from './compnents/EducationSection';
 import ExperienceSection from './compnents/ExperienceSection';
+import './styles/layout.css'
 
 function App() {
   return (
-    <main>
+    <main className='app-container'>
+      <section className='editor-container'>
       <h1>CV Builder</h1>
      <GeneralInfoSection />
      <EducationSection />
      <ExperienceSection />
+     </section>
+     <section className='preview-container'>
+      <h1>CV Preview</h1>
+      <p>YourCV preview will appear here.</p>
+
+     </section>
     </main>
   );
 }
