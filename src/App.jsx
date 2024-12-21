df45ac9ee5655ad4410a7fa33f043e48237de061
Graham4914 <67054735+Graham4914@users.noreply.github.com>
2024-12-21 14:56:27 +0800

    added print button and tweeked print output

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,19 @@ useEffect(() => {
      <section className='preview-container'>
       {/* <h1 className='preview-heading'>CV Preview</h1> */}
       <p>Your CV preview will appear here.</p>
+      <button className="print-button" onClick={() => window.print()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+            style={{ marginRight: "0.3rem" }}
+          >
+            <path d="M2 2v3h12V2H2zm2 9h8v3H4v-3zM1 7v4h3v2h8v-2h3V7H1z" />
+          </svg>
+          Print CV
+        </button>
       <CVPreview
       generalInfo={generalInfo}
       educationList={educationList}
